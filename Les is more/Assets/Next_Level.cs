b3a53bdd0b2cb5971fc189544b5cb22379fd3562
Joe Knapp <joe@Joes-MacBook-Pro.local>
2021-01-28 12:25:03 -0500

    working on level triggers

--- Les is more/Assets/Next_Level.cs
+++ Les is more/Assets/Next_Level.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class Next_Level : MonoBehaviour
 {
     public bool go_next_level = false;
+    public bool last_level = false;
+    public float level_count = 1;
+    public float x_axis_offset;
 
     // Start is called before the first frame update
     void Start()
@@ -23,6 +26,12 @@ public class Next_Level : MonoBehaviour
         if(collision.transform.tag == "Player")
         {
             go_next_level = true;
+            if(last_level == false)
+            {
+                transform.position = new Vector2(x_axis_offset, 4);
+                x_axis_offset += 3.7f;
+            }
+            level_count += 1f;
             Debug.Log("next level?");
         }
     }
